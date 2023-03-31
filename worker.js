4a7a8fe41ce043435ae9fb76e9528fe501bf8731
Jason Yu <jasonycw1992@gmail.com>
2023-03-31 10:41:26 +0800

    Also persist bot's conversation just in case

--- worker.js
+++ worker.js
@@ -35,21 +35,22 @@ async function handleRequest(request, env) {
     if (request.method === "POST") {
       const payload = await request.json();
       if (payload?.message?.from && !payload.message.from.is_bot) {
-        const chatId = payload.message.chat.id;
-        const text = `Hi ${payload.message.from.first_name} ${payload.message.from.last_name} @${payload.message.from.username}\nyour request payload is<pre><code class="language-json">${JSON.stringify(payload, null, 2)}</code></pre>`;
-
-        const url = `https://api.telegram.org/bot${env.API_KEY}/sendMessage?chat_id=${chatId}&text=${text}&parse_mode=HTML&reply_to_message_id=${payload.message.message_id}`;
-        await fetch(url);
-
         const kvKey = `conversation-with-${payload.message.from.username}`;
         const past = await env.KV.get(kvKey, { type: "json" });
         const kvValue = { ...past };
-        if (!kvValue.hasOwnProperty(payload.message.date))
-          kvValue[payload.message.date] = [payload.message.text];
-        else
-          kvValue[payload.message.date] = [...kvValue[payload.message.date], payload.message.text];
+        kvValue[payload.message.date] = kvValue.hasOwnProperty(payload.message.date)
+          ? [...kvValue[payload.message.date], payload.message.text]
+          : kvValue[payload.message.date] = [payload.message.text];
         await env.KV.put(kvKey, JSON.stringify(kvValue));
-        await logger.info(`Received msg from @${payload.message.from.username}.\nPayload:<pre><code class="language-json">${JSON.stringify(payload, null, 2)}</code></pre>`);
+
+        if (!payload.message.from.is_bot) {
+          const chatId = payload.message.chat.id;
+          const text = `Hi ${payload.message.from.first_name} ${payload.message.from.last_name} @${payload.message.from.username}\nyour request payload is<pre><code class="language-json">${JSON.stringify(payload, null, 2)}</code></pre>`;
+
+          const url = `https://api.telegram.org/bot${env.API_KEY}/sendMessage?chat_id=${chatId}&text=${text}&parse_mode=HTML&reply_to_message_id=${payload.message.message_id}`;
+          await fetch(url);
+          await logger.info(`Received msg from @${payload.message.from.username}.\nPayload:<pre><code class="language-json">${JSON.stringify(payload, null, 2)}</code></pre>`);
+        }
       }
     }
     return new Response("OK") // Doesn't really matter
