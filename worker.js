4fd3bec3a77f7d3734474eb19ee3410b7bae8e47
Jason Yu <jasonycw1992@gmail.com>
2023-04-01 00:46:26 +0800

    Change cron job from sending just 1 image to group media

--- worker.js
+++ worker.js
@@ -1,9 +1,26 @@
 export default {
   async scheduled(event, env, ctx) {
+    // const logger = new Logger(env);
     const now = (new Date()).toLocaleString('en-US', { timeZone: 'Asia/Hong_Kong' });
-    const photo = `https://www.hko.gov.hk/wxinfo/aws/hko_mica/ic2/latest_HD_IC2.jpg?v=${event.scheduledTime}`;
-    const url = `https://api.telegram.org/bot${env.API_KEY}/sendPhoto?chat_id=${env.TELEGRAM_LOG_CHAT_ID}&photo=${photo}&caption=CRON: \`${event.cron}\`\nFrom: [${now}](${photo})&parse_mode=MarkdownV2&disable_notification=true`;
-    await fetch(url);
+    const photos = {
+      'Victoria Peak (looking towards the north-northeast)': `https://www.hko.gov.hk/wxinfo/aws/hko_mica/vpb/latest_HD_VPB.jpg`,
+      'Central (Victoria Harbour)': `https://www.hko.gov.hk/wxinfo/aws/hko_mica/cp1/latest_HD_CP1.jpg`,
+      'International Commerce Centre (looking towards the southeast)': `https://www.hko.gov.hk/wxinfo/aws/hko_mica/ic1/latest_HD_IC1.jpg`,
+      'International Commerce Centre (looking towards the southwest)': `https://www.hko.gov.hk/wxinfo/aws/hko_mica/ic2/latest_HD_IC2.jpg`,
+      // 'Tsim Sha Tsui (looking towards the west)': `https://www.hko.gov.hk/wxinfo/aws/hko_mica/hk2/latest_HD_HK2.jpg`,
+      // 'Sai Wan Ho (looking towards the east)': `https://www.hko.gov.hk/wxinfo/aws/hko_mica/swh/latest_HD_SWH.jpg`,
+    }
+    const inputMediaPhotos = Object.keys(photos).map((location) => ({
+      type: 'photo',
+      media: `${photos[location]}?v=${event.scheduledTime}`,
+      caption: `${location}\n<pre>${now}</pre>`,
+      parse_mode: 'HTML'
+    }));
+    const url = `https://api.telegram.org/bot${env.API_KEY}/sendMediaGroup?chat_id=-39180215&media=${JSON.stringify(inputMediaPhotos)}&disable_notification=true`;
+    console.log(222, url);
+    const result = await fetch(url);
+    console.log(333, result);
+    //await logger.info(`Scheduled job from <code>${event.cron}</code> at ${now}.\n${photo}`);
   },
   async fetch(request, env) {
     return await handleRequest(request, env);
